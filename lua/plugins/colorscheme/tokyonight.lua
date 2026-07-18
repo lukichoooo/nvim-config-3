6721271286f59781f305c1e37339d17811163d12
Luka <khundadzeluka792@gmail.com>
2026-07-18 12:53:17 +0400

    fix tokyonight darker coloring issue on unused imports

--- lua/plugins/colorscheme/tokyonight.lua
+++ lua/plugins/colorscheme/tokyonight.lua
@@ -1,16 +1,19 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {
-    style = "night",
-on_colors = function(colors)
-  colors.bg = "#0d0d0d"
-  colors.bg_dark = "#090909"
-end,
-  },
-  config = function(_, opts)
-    require("tokyonight").setup(opts)
-    vim.cmd.colorscheme("tokyonight")
-  end,
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		style = "night",
+		on_colors = function(colors)
+			colors.bg = "#0d0d0d"
+			colors.bg_dark = "#090909"
+		end,
+		on_highlights = function(hl, _)
+			hl.DiagnosticUnderlineWarn = { fg = "#5a6a8a", undercurl = true, sp = "#e0af68" }
+		end,
+	},
+	config = function(_, opts)
+		require("tokyonight").setup(opts)
+		vim.cmd.colorscheme("tokyonight")
+	end,
 }
