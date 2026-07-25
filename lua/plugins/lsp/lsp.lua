5fbf1fb48cd08e9dbee6c857fe7bd71a6f55a2c7
Luka <khundadzeluka792@gmail.com>
2026-07-25 10:31:48 +0400

    fix c++ auto includes stuff

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -46,6 +46,18 @@ return {
 					},
 				},
 			})
+
+			vim.lsp.config.clangd = {
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy",
+					"--header-insertion=never", -- prevent auto including headers
+					"--completion-style=detailed",
+					"--function-arg-placeholders",
+					"--fallback-style=llvm",
+				},
+			}
 		end,
 	},
 }
