115ad3efdc481c2814f0f85bd337b4519dadd22a
Luka <khundadzeluka792@gmail.com>
2026-09-01 18:25:49 +0400

    easy dotnet config

--- lua/plugins/coding/easy-dotnet.lua
+++ lua/plugins/coding/easy-dotnet.lua
@@ -6,7 +6,18 @@ return {
 		"mfussenegger/nvim-dap",
 		"folke/snacks.nvim",
 	},
+	capabilities = {
+		workspace = {
+			didChangeWatchedFiles = {
+				dynamicRegistration = true,
+			},
+		},
+	},
 	config = function()
-		require("easy-dotnet").setup()
+		require("easy-dotnet").setup({
+			lsp = {
+				restart_roslyn_on_branch_change = true,
+			},
+		})
 	end,
 }
